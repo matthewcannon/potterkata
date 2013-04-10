aaaa316945e2db62f60d79bcf151ae1c328db354
matthew cannon <mcannon@ubuntu.(none)>
2013-04-10 11:50:08 +0100

    first passing test

--- test/the_test.rb
+++ test/the_test.rb
@@ -1,7 +1,23 @@
 require 'test/unit'
 
-class AddingNumbers < Test::Unit::TestCase
-	def test_one_plus_one_equals_two
-		assert_equal 2, 1 + 1
+class BuyingSingleBooks < Test::Unit::TestCase
+	def test_shopper_is_billed_8_for_buying_one_book
+		shopper = self
+		Shop.new(shopper).buy()
+		assert_equal @@bill, 8
+	end
+
+	def new_bill(bill)
+		@@bill = bill
+	end
+end
+
+class Shop
+	def initialize(shopper)
+		@@shopper = shopper 
+	end
+
+	def buy
+		@@shopper.new_bill(8)
 	end
 end
