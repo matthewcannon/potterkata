9e90f8718ec87a50a07c2b970ffc90c7560491eb
matthew cannon <mcannon@ubuntu.(none)>
2013-06-20 15:14:24 +0100

    add the second failing test

--- test/book_shop_test.rb
+++ test/book_shop_test.rb
@@ -5,17 +5,27 @@ class BuyingBooks < Test::Unit::TestCase
 		shopper = self
 		price_of_one_book = 8
 
-		Shop.new(shopper, price_of_one_book).buy()
+		BookShop.new(shopper, price_of_one_book).buy()
 
-		assert_equal @@bill, price_of_one_book
+		assert_equal(@@bill, price_of_one_book)
 	end
 
 	def new_bill(bill)
 		@@bill = bill
 	end
+
+	def test_when_buying_two_of_the_same_book_shopper_is_billed_price_of_two_books
+		shopper = self
+		price_of_one_book = 8
+		price_of_two_books = price_of_one_book * 2
+
+		BookShop.new(shopper, price_of_one_book).buy()
+
+		assert_equal(@@bill, price_of_two_books)
+	end
 end
 
-class Shop
+class BookShop
 	def initialize(shopper, book_price)
 		@@shopper = shopper 
 		@@book_price = book_price
